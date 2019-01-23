b1761c4251ae60a34bc04972a76cabf74d74cded
carolinefeeney <46456081+carolinefeeney@users.noreply.github.com>
2019-01-23 16:56:24 -0500

    Find matching products

--- groceries.py
+++ groceries.py
@@ -69,10 +69,10 @@ print("--------------")
 
 unique_departments.sort()
 
-
-
 for d in unique_departments:
-    print(d)
+    matching_products = [p for p in products if p["department"] == d]
+    matching_products_count = len(matching_products)
+    print(d.title() + " (" str(matching_products_count) + " products)")
 
 
 
